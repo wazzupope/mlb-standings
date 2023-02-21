36516d2e4a70417ddb8def3b33e5f4b6196e9871
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-20 22:53:16 -0800

    add fetch statement, awaiting API authorization for url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("mlb players soon");
+fetch("x", {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+  });
