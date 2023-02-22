c44e8ed2be07875536dca14cef066283aeab1d93
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-21 23:51:33 -0800

    modify fetch request to get data from soccer api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-fetch("x", {mode: 'cors'})
+const myHeaders = new Headers();
+myHeaders.append('x-apisports-key', 'c16bc477f3a2a8d3fd289b58d1172ad8');
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow',
+};
+
+fetch("https://v3.football.api-sports.io/standings?season=2022&league=39", requestOptions, {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
