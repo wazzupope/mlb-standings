3720447f410c38f8250a23743b4fbdcce9676278
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-24 00:32:27 -0800

    import loadHome function from home.js, update fetch to be able to print team name to main element

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+import {loadHome} from './home';
+
+loadHome();
+
+// API headers
 const myHeaders = new Headers();
 myHeaders.append('x-apisports-key', 'c16bc477f3a2a8d3fd289b58d1172ad8');
 
+// API options
 const requestOptions = {
   method: 'GET',
   headers: myHeaders,
   redirect: 'follow',
 };
 
+// Fetch request to API for MLB standings
 fetch("https://v1.baseball.api-sports.io/standings?season=2023&league=1", requestOptions, {mode: 'cors'})
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then(result => {
+    const main = document.getElementById('main');
+    main.textContent = result.response[0][0].team.name;
+  })
   .catch(error => console.log('error', error));
