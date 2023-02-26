897fdad447cb3bddb0fafc57b88b63cd34441dec
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-26 08:18:09 -0800

    add todo list, working toward making each team in array an object of class type Team, incomplete process but needed to stop and save

--- src/index.js
+++ src/index.js
@@ -13,21 +13,47 @@ const requestOptions = {
   redirect: 'follow',
 };
 
-// Fetch request to API for MLB standings
-async function getData() {
-  const response = await fetch("https://v1.baseball.api-sports.io/standings?season=2023&league=1", requestOptions, {mode: 'cors'})
-    const result = await response.json();
-    return result;
-
-    // .then(response => response.json())
-    // .then(result => {
-    //   const main = document.getElementById('main');
-    //   main.textContent = result.response[0][0].team.name;
-    //   return result;
-    // })
-    // .catch(error => console.log('error', error));
+class Team {
+  constructor(name, id, logo, runsFor, runsAgainst, league, gamesPlayed, gamesWon, gamesLost) {
+    this.name = name;
+    this.id = id;
+    this.logo = logo;
+    this.runsFor = runsFor;
+    this.runsAgainst = runsAgainst;
+    this.league = league;
+    this.gamesPlayed = gamesPlayed;
+    this.gamesWon = gamesWon;
+    this.gamesLost = gamesLost;
+  };
 };
 
-const data = getData();
+/*
+TODO
+look at function below and figure out how to take array itsms in teams (in fetch call)
+and make each of them an object of class Team
 
-console.log(data);
+figure out how to handle fetch error in .catch (article on phone)
+*/
+
+function createTeams(teams) {
+  const teamArray[] = 
+  for()
+  teams[i] = new Team(teams[i].name, etc.)
+}
+
+// Fetch request to API for MLB standings
+fetch("https://v1.baseball.api-sports.io/standings?season=2023&league=1", requestOptions, {mode: 'cors'})
+  .then(response => response.json())
+  .then(result => {
+    const teams = result.response[0];
+    return teams;
+  })
+  .then(teams => {
+    teams.forEach(
+
+    )
+  })
+    // const main = document.getElementById('main');
+    // main.textContent = result.response[0][0].team.name;
+    // return result;
+  .catch(error => console.log('error', error));
